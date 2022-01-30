216076e977f23ffbe0c72f2158afe5c7c02644f2
Trinethr N <trinethr_n@protonmail.com>
2022-01-30 21:23:18 +0530

    Added files

---
+++ src/index.js
@@ -0,0 +1,120 @@
+const electron = require('electron');
+const glasstron = require('glasstron');
+const path = require('path');
+const { ipcMain } = require('electron');
+const fs = require('fs');
+
+const datapath = electron.app.getPath('userData');
+console.log(datapath);
+
+if (fs.existsSync(datapath + (process.platform == 'win32' ? "\\" : "/") + "todo.txt")) {
+  fs.readFile(datapath + (process.platform == 'win32' ? "\\" : "/") + "todo.txt", 'utf-8', (err, data) => {
+    if(err){
+      console.log("An error ocurred reading the file :" + err.message);
+    }
+    var todocontent = data;
+    ipcMain.on('request-mainprocess-action', (event, arg) => {
+      event.sender.send('mainprocess-response', todocontent);
+    });
+  });
+}
+else {
+  fs.writeFile(datapath + (process.platform == 'win32' ? "\\" : "/") + "todo.txt", '', (err) => {
+    if(err){
+      console.log("An error ocurred creating the file "+ err.message)
+    }
+    console.log("New todo file created");
+  });
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
+  electron.app.quit();
+}
+electron.app.commandLine.appendSwitch("enable-transparent-visuals");
+electron.app.on('ready', () => {
+  setTimeout(
+    spawnWindow,
+    process.platform == "linux" ? 1000 : 0
+    // Electron has a bug on linux where it
+    // won't initialize properly when using
+    // transparency. To work around that, it
+    // is necessary to delay the window
+    // spawn function.
+  );
+});
+const spawnWindow = () => {
+  // Create the browser window.
+  const win = new glasstron.BrowserWindow({
+    width: 400,
+    height: 800,
+    minWidth: 400,
+    frame: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+
+  // and load the index.html of the app.
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  win.blurType = "blurbehind";
+  win.setBlur(true);
+
+  electron.ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } 
+    else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  });
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  });
+  ipcMain.on('close-win', (event, arg) => {
+    win.close();
+  });
+  ipcMain.on('minimize-win', (event, arg) => {
+    win.minimize();
+  });
+};
+
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+electron.app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    electron.app.quit();
+  }
+});
+
+electron.app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    spawnWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+ipcMain.on('savefile', (event, arg) => {
+  fs.writeFile(datapath + (process.platform == 'win32' ? "\\" : "/") + "todo.txt", arg, (err) => {
+    if(err){
+      console.log("An error ocurred creating the file "+ err.message)
+    }
+    console.log("The file has been succesfully saved");
+  });
+});
+  ipcMain.on('request-datapath', (event) => {
+  event.sender.send('datapath', datapath);
+});
---
+++ src/render.js
@@ -0,0 +1,178 @@
+const { ipcRenderer } = require('electron');
+const storage = require('electron-json-storage');
+const os = require('os');
+
+var inputbar = document.getElementById('input');
+inputbar.addEventListener("keyup", function(EnterPressed) {
+  if (EnterPressed.keyCode === 13) {
+    EnterPressed.preventDefault();
+    newItem();
+  }
+});
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.remove();
+    ipcRenderer.send('savefile', content);
+  }
+}
+var listItem = document.querySelector('ul');
+listItem.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('done');
+    var content = document.getElementById("TodoList").innerHTML;
+    ipcRenderer.send('savefile', content);
+  }
+}, false);
+function newItem() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } 
+  else {
+    document.getElementById("TodoList").appendChild(li);
+    document.getElementById("input").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    var content = document.getElementById("TodoList").innerHTML;
+    ipcRenderer.send('savefile', content);
+  }
+  
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.remove();
+      ipcRenderer.send('savefile', content);
+    }
+  }
+}
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;  
+    let time = hh + ":" + mm
+    document.getElementById("time").innerText = "The time is " + time; 
+    let t = setTimeout(function(){ currentTime() }, Infinity);
+}
+function currentDate() {
+    let today = new Date();
+    let day = today.getDay();
+    if (day == 0) {dayName = "Sunday"}
+    if (day == 1) {dayName = "Monday"}
+    if (day == 2) {dayName = "Tuesday"}
+    if (day == 3) {dayName = "Wednesday"}
+    if (day == 4) {dayName = "Thursday"}
+    if (day == 5) {dayName = "Friday"}
+    if (day == 6) {dayName = "Saturday"}
+    let dayNum = today.getDate();
+    if (dayNum == 1 || dayNum == 21 || dayNum == 31) {suffix = "st"}
+    if (dayNum == 2 || dayNum == 22) {suffix = "nd"}
+    if (dayNum == 3 || dayNum == 23) {suffix = "rd"}
+    if (dayNum >= 4 && dayNum != 31 && dayNum != 23 && dayNum != 22 && dayNum != 21) {suffix = "th"}
+    let month = today.getMonth();
+    if (month == 0) {monthName = " January"}
+    if (month == 1) {monthName = " February"}
+    if (month == 2) {monthName = " March"}
+    if (month == 3) {monthName = " April"}
+    if (month == 4) {monthName = " May"}
+    if (month == 5) {monthName = " June"}
+    if (month == 6) {monthName = " July"}
+    if (month == 7) {monthName = " August"}
+    if (month == 8) {monthName = " September"}
+    if (month == 9) {monthName = " October"}
+    if (month == 10) {monthName = " November"}
+    if (month == 11) {monthName = " December"}
+    let year = " " + today.getFullYear();
+    let date = dayName + ", " + dayNum + suffix + monthName + year;
+    document.getElementById("date").innerText = "Today is " + date;
+    let t = setTimeout(function(){ currentTime() }, Infinity); 
+}
+currentTime();
+currentDate();
+let css = document.querySelector(':root');
+let open = "0";
+function sidebar() {
+  if (open == "0") {
+    document.getElementById("sidebar").style.width = "250px";
+    css.style.setProperty('--filter', 'blur(5px)');
+    css.style.setProperty('--titlebar-bg', '#121212cc');
+    open = "1";
+  }
+  else if (open == "1") {
+    document.getElementById("sidebar").style.width = "0";
+    css.style.setProperty('--filter', 'none');
+    css.style.setProperty('--titlebar-bg', '#12121255');
+    open = "0";
+  }
+}
+
+ipcRenderer.on('filepath', (event, arg) => {
+  console.log(arg);
+});
+
+ipcRenderer.send('request-mainprocess-action');
+
+ipcRenderer.on('mainprocess-response', (event, arg) => {
+  document.getElementById("TodoList").insertAdjacentHTML("beforeend", arg);
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.remove();
+      ipcRenderer.send('savefile', content);
+    }
+  }
+});
+ipcRenderer.send('request-datapath');
+ipcRenderer.on('datapath', (event, arg) => {
+  //configfolder = 
+  storage.setDataPath(arg + (process.platform == 'win32' ? "\\" : "/") + "config");
+  storage.get('opacity', function(error, data) {
+    if (error) throw error;
+    for(let value of Object.values(data)){
+      console.log(value)
+      css.style.setProperty('--bg-opacity', value);
+    }
+  });
+});
+var opacitybar = document.getElementById('opacity-input');
+function ChangeOpacity() {
+  opacity = opacitybar.value;
+  if (opacity <=1 && opacity >= 0) {
+    css.style.setProperty('--bg-opacity', opacity);
+    opacitybar.placeholder = opacity;
+    opacitybar.value = '';
+    storage.set('opacity', {"opacity" : opacity}, function(error) {
+      if (error) throw error;
+    });
+  }
+  else {
+    alert("Invalid opacity. Please supply a number between 0 and 1.");
+  }
+}
+opacitybar.addEventListener("keyup", function(EnterPressed) {
+  if (EnterPressed.keyCode === 13) {
+    EnterPressed.preventDefault();
+    ChangeOpacity();
+  }
+});
+function closeWin() {
+  ipcRenderer.send('close-win');
+}
+function minimizeWin() {
+  ipcRenderer.send('minimize-win');
+}
