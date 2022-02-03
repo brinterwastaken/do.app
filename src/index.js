ce3f889f3730337b3f85101edfe344e2e38be4cd
Trinethr N <trinethr_n@protonmail.com>
2022-02-03 12:24:20 +0530

    Trying to fix blur on macos + added acrylic blur on windows

--- src/index.js
+++ src/index.js
@@ -58,19 +58,23 @@ const spawnWindow = () => {
       contextIsolation: false,
     }
   });
-
+  console.log("do.app on " + process.platform);
 
   // and load the index.html of the app.
   win.loadFile(path.join(__dirname, 'index.html'));
-
-  if (process.platform == 'darwin') {
-    win.vibrancy;
+  if (process.platform == "win32") {
+    win.blurType = "acrylic";
+    acrylicWorkaround(win, 60);
+    win.setBlur(true);
   }
-  else {
+  else if (process.platform == "linux") {
     win.blurType = "blurbehind";
+    win.setBlur(true);
   }
-
-  win.setBlur(true);
+  else if (process.platform == "darwin") {
+    win.setVibrancy('fullscreen-ui');
+    win.setBlur(true);
+  }
 
   ipcMain.handle('dark-mode:disabled', () => {
     electron.nativeTheme.themeSource = 'light'
@@ -121,3 +125,70 @@ ipcMain.on('savefile', (event, arg) => {
   ipcMain.on('request-datapath', (event) => {
   event.sender.send('datapath', datapath);
 });
+
+
+// Acrylic Workaround from https://github.com/NyaomiDEV/Glasstron/blob/master/test/index.js
+function acrylicWorkaround(win, pollingRate = 60){
+  // Replace window moving behavior to fix mouse polling rate bug
+  win.on("will-move", (e) => {
+    if(win.blurType !== "acrylic")
+      return;
+    
+    e.preventDefault();
+
+    // Track if the user is moving the window
+    if(win._moveTimeout)
+      clearTimeout(win._moveTimeout);
+
+    win._moveTimeout = setTimeout(
+      () => {
+        win._isMoving = false;
+        clearInterval(win._moveInterval);
+        win._moveInterval = null;
+      }, 1000/pollingRate);
+
+    // Start new behavior if not already
+    if(!win._isMoving){
+      win._isMoving = true;
+      if(win._moveInterval)
+        return false;
+
+      // Get start positions
+      win._moveLastUpdate = 0;
+      win._moveStartBounds = win.getBounds();
+      win._moveStartCursor = electron.screen.getCursorScreenPoint();
+
+      // Poll at (refreshRate * 10) hz while moving window
+      win._moveInterval = setInterval(() => {
+        const now = Date.now();
+        if(now >= win._moveLastUpdate + (1000/pollingRate)){
+          win._moveLastUpdate = now;
+          const cursor = electron.screen.getCursorScreenPoint();
+
+          // Set new position
+          win.setBounds({
+            x: win._moveStartBounds.x + (cursor.x - win._moveStartCursor.x),
+            y: win._moveStartBounds.y + (cursor.y - win._moveStartCursor.y),
+            width: win._moveStartBounds.width,
+            height: win._moveStartBounds.height
+          });
+        }
+      }, 1000/(pollingRate * 10));
+    }
+  });
+
+  // Replace window resizing behavior to fix mouse polling rate bug
+  win.on("will-resize", (e) => {
+    if(win.blurType !== "acrylic")
+      return;
+
+    const now = Date.now();
+    if(!win._resizeLastUpdate)
+      win._resizeLastUpdate = 0;
+
+    if(now >= win._resizeLastUpdate + (1000/pollingRate))
+      win._resizeLastUpdate = now;
+    else
+      e.preventDefault();
+  });
+}
