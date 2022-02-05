880307380fca8cbf07aed5e53e7b260dbcf1a4c5
Trinethr N <trinethr_n@protonmail.com>
2022-02-05 10:25:38 +0530

    Fixed blur on macos

--- src/index.js
+++ src/index.js
@@ -129,7 +129,7 @@ electron.app.on('window-all-closed', () => {
 electron.app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+  if (glasstron.BrowserWindow.getAllWindows().length === 0) {
     spawnWindow();
   }
 });
